5c7b14f4c3b6cb85743725d482097f89764702f0
henrique97-hub <61150198+henrique97-hub@users.noreply.github.com>
2020-09-16 20:32:46 -0300

    Enumeração
    Foi criada a enumeração referente aos tipos de raridade das cartas.

---
+++ src/maua/br/enumeracoes/PkmRaridade.java
@@ -0,0 +1,35 @@
+package maua.br.enumeracoes;
+
+public enum PkmRaridade {
+    COMUM {
+    @Override
+    public String toString(){ return "Pokémon do tipo comum";}
+},
+
+    INCOMUM {
+    @Override
+    public String toString(){return "Pokémon do tipo incomum";}
+},
+    RARO {
+        @Override
+        public String toString() {return "Pokémon do tipo raro";}
+
+    },
+    MUITO_RARO {
+        @Override
+        public String toString() {return "Pokémon do tipo muito raro";}
+    },
+    EPICO {
+        @Override
+        public String toString() { return "Pokémon do tipo épico";}
+
+        },
+    LENDARIO {
+        @Override
+        public String toString() { return "Pokémon do tipo lendário";}
+
+        }
+    }
+
+
+
--- src/maua/br/model/Carta.java
+++ src/maua/br/model/Carta.java
@@ -1,5 +1,12 @@
 package maua.br.model;
 
 public class Carta {
-    
+    private String url;
+    private String id;
+    private String nome;
+    private String raridade;
+    private String serie;
+    private String colecao;
+
+
 }
